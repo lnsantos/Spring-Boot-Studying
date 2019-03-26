f8675ab2aa120de4483df528eba80dd6c5a86243
lucasdosenai <39661894+lucasdosenai@users.noreply.github.com>
2019-03-26 16:54:42 -0300

    Progredindo

--- src/main/java/com/lucasdosenai/resources/CategoriaRecurso.java
+++ src/main/java/com/lucasdosenai/resources/CategoriaRecurso.java
@@ -1,8 +1,13 @@
 package com.lucasdosenai.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lucasdosenai.domain.Categoria;
 
 // 1° COLOCA ANOTAÇÃO
 @RestController
@@ -10,9 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaRecurso {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!!";
-	
+	public List<Categoria> listar() {
+		
+		Categoria ctg1 = new Categoria(1, "Informática");
+		Categoria ctg2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listaC = new ArrayList<>();
+		listaC.add(ctg1);
+		listaC.add(ctg2);
+		
+		return listaC;
 	}
 	
 }
