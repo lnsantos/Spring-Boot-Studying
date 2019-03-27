3adadade761dc1012fd59974bbe761371d9a42d1
lucasdosenai <39661894+lucasdosenai@users.noreply.github.com>
2019-03-27 00:56:51 -0300

    H2 Inserido

--- src/main/java/com/lucasdosenai/domain/Categoria.java
+++ src/main/java/com/lucasdosenai/domain/Categoria.java
@@ -2,10 +2,19 @@ package com.lucasdosenai.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 // Serializable indica que os Objetos dela pode ser convertido em bit
+@Entity
 public class Categoria implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
